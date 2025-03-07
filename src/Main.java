b1a2935faa04deb4188bed81dbd599f142f661bb
NikolayO <n9056446438@gmail.com>
2025-03-07 22:38:43 +0300

    Fixes did.

--- src/Main.java
+++ src/Main.java
@@ -12,67 +12,80 @@ public class Main {
             System.out.println("i = " + i);
         }
 
-        // Task 1
+        System.out.println("TASK 1");
+
         var dog = 8.0;
         var cat = 3.6;
         var paper = 763789;
 
-        System.out.println("TASK 1");
         System.out.println("Dog value: " + dog);
         System.out.println("Cat value: " + cat);
         System.out.println("Paper value: " + paper);
 
-        // Task 2
+        System.out.println("TASK 2");
+
         dog = dog + 4;
         cat = cat + 4;
         paper = paper + 4;
 
-        System.out.println("TASK 2");
         System.out.println("Dog value: " + dog);
         System.out.println("Cat value: " + cat);
         System.out.println("Paper value: " + paper);
 
-        // Task 3
+        System.out.println("TASK 3");
+
         dog = dog - 3.5;
         cat = cat - 1.6;
         paper = paper - 7639;
 
-        System.out.println("TASK 3");
         System.out.println("Dog value: " + dog);
         System.out.println("Cat value: " + cat);
         System.out.println("Paper value: " + paper);
 
-        // Task 4
+        System.out.println("TASK 4");
+
         var friend = 19;
 
-        System.out.println("TASK 4");
         System.out.println(friend);
-        System.out.println(friend = friend + 2);
-        System.out.println(friend / 7);
-
-        // Task 5
-        var frog = 3.5;
+        friend = friend + 2;
+        System.out.println(friend);
+        friend = friend / 7;
+        System.out.println(friend);
 
         System.out.println("TASK 5");
-        System.out.println(frog);
-        System.out.println(frog = frog * 10);
-        System.out.println(frog = frog / 3.5);
-        System.out.println(frog + 4);
 
-        // Task 6
+        var frog = 3.5;
+
+        System.out.println(frog);
+        frog = frog * 10;
+        System.out.println(frog);
+        frog = frog / 3.5;
+        System.out.println(frog);
+        frog = frog + 4;
+        System.out.println(frog);
+
         System.out.println("TASK 6");
 
-        var guy1 = 78.2;
-        var guy2 = 82.7;
+        var guyOne = 78.2;
+        var guyTwo = 82.7;
+        var massTotal = guyOne + guyTwo;
+        var massDiff = Math.abs(guyOne - guyTwo);
 
-        System.out.println("Mass total: " + (guy1 + guy2) + " kg");
-        System.out.println("Mass diff: " + (Math.abs(guy1 - guy2)) + " kg");
+        System.out.println("Mass total: " + massTotal + " kg");
+        System.out.println("Mass diff: " + massDiff + " kg");
 
-        // Task 7
         System.out.println("TASK 7");
-        System.out.println(guy1 > guy2 ? guy1 % guy2 : guy2 % guy1);
 
-        // Task 8
+        double remainderOfDivision;
+
+        if (guyOne > guyTwo) {
+            remainderOfDivision = guyOne % guyTwo;
+        } else {
+            remainderOfDivision = guyTwo % guyOne;
+        }
+
+        System.out.println(remainderOfDivision);
+
         System.out.println("TASK 8");
 
         var totalCapacity = 640;
